a2f2fb422c4e74ddd0e4c3a3ae413417d9e3d03d
Raj Dutta <rd2249619@gmail.com>
2022-07-15 00:17:26 +0600

    setup database connection

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log('Hello World!');
+const Database = require("./src/config/db.config");
+
+Database.connect().then((obj) => console.log('database is successfully connected.', obj)).catch((err) => console.log('failed to connect with the database', err));
